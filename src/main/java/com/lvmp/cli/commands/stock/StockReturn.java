d2812aab45b588eb07cfcf5b3b05db3d36e77105
Lesedi <molemilesedi@gmail.com>
2025-12-06 11:57:07 +0200

    feat: implement getReturns in the command and add ticker option

--- src/main/java/com/lvmp/cli/commands/stock/StockReturn.java
+++ src/main/java/com/lvmp/cli/commands/stock/StockReturn.java
@@ -1,6 +1,9 @@
 package com.lvmp.cli.commands.stock;
 
+import com.lvmp.polygon.model.StockReturnsResponse;
+import com.lvmp.polygon.service.PolygonService;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 import java.util.concurrent.Callable;
 
@@ -10,9 +13,22 @@ import java.util.concurrent.Callable;
 )
 public class StockReturn implements Callable<Integer> {
 
+    @Option (names = {"-t", "--ticker"}, required = true)
+    private String ticker;
+
     @Override
     public Integer call() {
-        System.out.println("This is the stock return sub-command.");
+        try {
+            StockReturnsResponse result = PolygonService.getReturns(ticker);
+
+            System.out.println("Current price: " + result.currentPrice);
+            System.out.println("1 Month Return: " + result.oneMonthReturn + "%");
+            System.out.println("6 Month Return: " + result.sixMonthReturn + "%");
+            System.out.println("Year-To-Date: " + result.yearToDate + "%");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return 1;
+        }
         return 0;
     }
 }
